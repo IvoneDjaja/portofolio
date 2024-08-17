0be151f528027381ef2ed5ec39da4e1f011bc9a1
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-08-17 21:07:45 +0800

    :wrench: Add pubspec.yaml

--- apps/portofolio/app.dart
+++ apps/portofolio/app.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'data/gallery_options.dart';
 import 'routes.dart';
 import 'themes/gallery_theme_data.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 /// The Widget that configures your application.
 class PortofolioApp extends StatelessWidget {
