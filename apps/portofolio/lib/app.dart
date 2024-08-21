813eeb23d8414a52504965862f4ff56fbd188480
IvoneDjaja <mariaivoneiradjaja@gmail.com>
2024-08-21 16:27:03 +0800

    :fire: Remove localization

--- apps/portofolio/lib/app.dart
+++ apps/portofolio/lib/app.dart
@@ -3,7 +3,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:portofolio/routes.dart';
 import '../data/gallery_options.dart';
 import '../themes/gallery_theme_data.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 /// The Widget that configures your application.
 class PortofolioApp extends StatelessWidget {
@@ -30,7 +29,6 @@ class PortofolioApp extends StatelessWidget {
           // allows descendant Widgets to display the correct translations
           // depending on the user's locale.
           localizationsDelegates: const [
-            AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
@@ -38,14 +36,6 @@ class PortofolioApp extends StatelessWidget {
           supportedLocales: const [
             Locale('en', ''), // English, no country code
           ],
-
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
           themeMode: options.themeMode,
           theme: GalleryThemeData.lightThemeData,
           darkTheme: GalleryThemeData.darkThemeData,
