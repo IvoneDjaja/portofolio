479db936901eb7f2d746904e58f4abcc85c681de
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-22 23:23:05 +0800

    :recycle: Set project structure as feature first

--- apps/ecommerce_starter_app/lib/src/app.dart
+++ apps/ecommerce_starter_app/lib/src/app.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_starter_app/src/features/products_list/products_list_screen.dart';
+import 'package:ecommerce_starter_app/src/features/products/presentation/products_list/products_list_screen.dart';
 import 'package:ecommerce_starter_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_starter_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
