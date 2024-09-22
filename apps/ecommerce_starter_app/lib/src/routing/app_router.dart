1b090a311d722ee27033a57981834cdd30dff4a2
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-22 18:07:04 +0800

    :sparkles: Add routing by name

--- apps/ecommerce_starter_app/lib/src/routing/app_router.dart
+++ apps/ecommerce_starter_app/lib/src/routing/app_router.dart
@@ -7,16 +7,27 @@ import 'package:ecommerce_starter_app/src/features/sign_in/email_password_sign_i
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+enum AppRoute {
+  home,
+  product,
+  cart,
+  orders,
+  account,
+  signIn,
+}
+
 final goRouter = GoRouter(
   initialLocation: '/',
   debugLogDiagnostics: true,
   routes: [
     GoRoute(
       path: '/',
+      name: AppRoute.home.name,
       builder: (context, state) => ProductsListScreen(),
       routes: [
         GoRoute(
           path: 'cart',
+          name: AppRoute.cart.name,
           builder: (context, state) => ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
@@ -26,6 +37,7 @@ final goRouter = GoRouter(
         ),
         GoRoute(
           path: 'orders',
+          name: AppRoute.orders.name,
           builder: (context, state) => ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
@@ -35,6 +47,7 @@ final goRouter = GoRouter(
         ),
         GoRoute(
           path: 'account',
+          name: AppRoute.account.name,
           builder: (context, state) => ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
@@ -44,6 +57,7 @@ final goRouter = GoRouter(
         ),
         GoRoute(
           path: 'signIn',
+          name: AppRoute.signIn.name,
           builder: (context, state) => ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
