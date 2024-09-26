c59435242e4a1222a705c47c34f5457e761cafd9
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-26 21:41:25 +0800

    :sparkles: Read providers with ConsumerWidget and Consumer

--- apps/ecommerce_starter_app/lib/src/features/products/presentation/products_list/products_grid.dart
+++ apps/ecommerce_starter_app/lib/src/features/products/presentation/products_list/products_grid.dart
@@ -1,24 +1,22 @@
 import 'dart:math';
-
-import 'package:ecommerce_starter_app/src/constants/test_products.dart';
 import 'package:ecommerce_starter_app/src/features/products/data/fake_products_repository.dart';
-import 'package:ecommerce_starter_app/src/features/products/presentation/product_screen/product_screen.dart';
 import 'package:ecommerce_starter_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_starter_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:ecommerce_starter_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_starter_app/src/features/products/presentation/products_list/product_card.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 /// A widget that displays the list of products that match the search query.
-class ProductsGrid extends StatelessWidget {
+class ProductsGrid extends ConsumerWidget {
   const ProductsGrid({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Read from data source
-    final products = FakeProductsRepository.instance.getProductsList();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsRepository = ref.watch(productsRepositoryProvider);
+    final products = productsRepository.getProductsList();
     return products.isEmpty
         ? Center(
             child: Text(
