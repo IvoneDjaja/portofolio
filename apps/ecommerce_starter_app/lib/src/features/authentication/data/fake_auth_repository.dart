bcfd293cfda81830bd3957b9a985acc7096200a3
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-28 10:53:36 +0800

    :white_check_mark: Add test FakeAuthRepository

--- apps/ecommerce_starter_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ apps/ecommerce_starter_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,21 +1,30 @@
 import 'package:ecommerce_starter_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_starter_app/src/utils/delay.dart';
 import 'package:ecommerce_starter_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeAuthRepository {
+  FakeAuthRepository({this.addDelay = true});
+  final bool addDelay;
   final _authState = InMemoryStore<AppUser?>(null);
 
   Stream<AppUser?> authStateChanges() => _authState.stream;
   AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
+    await delay(addDelay);
     if (currentUser == null) {
       _createNewUser(email);
     }
   }
 
   Future<void> createUserWithEmailAndPassword(
-      String email, String password) async {}
+      String email, String password) async {
+    await delay(addDelay);
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
 
   Future<void> signOut() async {
     _authState.value = null;
---
+++ apps/ecommerce_starter_app/test/src/features/authentication/data/fake_auth_repository_test.dart
@@ -0,0 +1,28 @@
+import 'package:ecommerce_starter_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_starter_app/src/features/authentication/domain/app_user.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const testEmail = 'test@test.com';
+  final testPassword = '1234';
+  final testUser = AppUser(
+    uid: testEmail.split('').reversed.join(),
+    email: testEmail,
+  );
+  FakeAuthRepository makeAuthRepository() =>
+      FakeAuthRepository(addDelay: false);
+  group('FakeProductsRepository', () {
+    test('currentUser is null', () {
+      final authRepository = FakeAuthRepository();
+      expect(authRepository.currentUser, null);
+      expect(authRepository.authStateChanges(), emits(null));
+    });
+
+    test('currentUser is not null after sign in', () async {
+      final authRepository = makeAuthRepository();
+      await authRepository.signInWithEmailAndPassword(testEmail, testPassword);
+      expect(authRepository.currentUser, testUser);
+      expect(authRepository.authStateChanges(), emits(testUser));
+    });
+  });
+}
