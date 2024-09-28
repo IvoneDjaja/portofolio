b2792866a8e3a675ccbdfb60d2c67c10e1f732a6
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-28 18:04:08 +0800

    :white_check_mark: Add test find widgets by key

--- apps/ecommerce_starter_app/test/src/features/authentication/presentation/account/account_screen_test.dart
+++ apps/ecommerce_starter_app/test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -14,4 +14,13 @@ void main() {
     await r.tapCancelButton();
     r.expectLogoutDialogNotFound();
   });
+
+  testWidgets('Confirm logout success', (tester) async {
+    final r = AuthRobot(tester);
+    await r.pumpAccountScreen();
+    await r.tapLogoutButton();
+    r.expectLogoutDialogFound();
+    await r.tapDialogLogoutButton();
+    r.expectLogoutDialogNotFound();
+  });
 }
