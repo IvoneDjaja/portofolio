a5b653e5cf6716b0a0496e4a8369afab3cf6566f
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-28 17:52:37 +0800

    :white_check_mark: Add robot testing

--- apps/ecommerce_starter_app/test/src/features/authentication/presentation/account/account_screen_test.dart
+++ apps/ecommerce_starter_app/test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -3,25 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../auth_robot.dart';
+
 void main() {
   testWidgets('Cancel logout', (tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
-          home: AccountScreen(),
-        ),
-      ),
-    );
-    final logoutButton = find.text('Logout');
-    expect(logoutButton, findsOneWidget);
-    await tester.tap(logoutButton);
-    await tester.pump();
-    final dialogTitle = find.text('Are you sure?');
-    expect(dialogTitle, findsOneWidget);
-    final cancelButton = find.text('Cancel');
-    expect(cancelButton, findsOneWidget);
-    await tester.tap(cancelButton);
-    await tester.pump();
-    expect(dialogTitle, findsNothing);
+    final r = AuthRobot(tester);
+    await r.pumpAccountScreen();
+    await r.tapLogoutButton();
+    r.expectLogoutDialogFound();
+    await r.tapCancelButton();
+    r.expectLogoutDialogNotFound();
   });
 }
