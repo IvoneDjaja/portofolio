5ba3a5e0ac863ba2b549378f016ad307f7a77e86
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-28 09:10:43 +0800

    :white_check_mark: Add unit test, toString(), and equality implementations

---
+++ apps/ecommerce_starter_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -0,0 +1,13 @@
+import 'package:ecommerce_starter_app/src/constants/test_products.dart';
+import 'package:ecommerce_starter_app/src/features/products/data/fake_products_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('getProductsList returns global list', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      productsRepository.getProductsList(),
+      kTestProducts,
+    );
+  });
+}
