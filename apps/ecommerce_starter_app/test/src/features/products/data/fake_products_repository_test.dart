3e1e2a46e38edd5b38ebd0402d2cad61408bcab7
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-28 10:05:47 +0800

    :white_check_mark: Add group and test future and stream

--- apps/ecommerce_starter_app/test/src/features/products/data/fake_products_repository_test.dart
+++ apps/ecommerce_starter_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -3,27 +3,61 @@ import 'package:ecommerce_starter_app/src/features/products/data/fake_products_r
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('getProductsList returns global list', () {
-    final productsRepository = FakeProductsRepository();
-    expect(
-      productsRepository.getProductsList(),
-      kTestProducts,
-    );
-  });
+  group('FakeProductsRepository', () {
+    test('getProductsList returns global list', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.getProductsList(),
+        kTestProducts,
+      );
+    });
 
-  test('getProduct(1) returns first item', () {
-    final productsRepository = FakeProductsRepository();
-    expect(
-      productsRepository.getProduct('1'),
-      kTestProducts[0],
-    );
-  });
+    test('getProduct(1) returns first item', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.getProduct('1'),
+        kTestProducts[0],
+      );
+    });
 
-  test('getProduct(100) returns null', () {
-    final productsRepository = FakeProductsRepository();
-    expect(
-      () => productsRepository.getProduct('100'),
-      throwsStateError,
-    );
+    test('getProduct(100) returns null', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        () => productsRepository.getProduct('100'),
+        throwsStateError,
+      );
+    });
+
+    test('fetchProductsList returns global list', () async {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        await productsRepository.fetchProductsList(),
+        kTestProducts,
+      );
+    });
+
+    test('watchProductsList emits global list', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.watchProductsList(),
+        emits(kTestProducts),
+      );
+    });
+
+    test('watchProduct(1) emits first item', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.watchProduct('1'),
+        emits(kTestProducts[0]),
+      );
+    });
+
+    test('watchProduct(100) emits null', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.watchProduct('100'),
+        emits(null),
+      );
+    });
   });
 }
