98b80d33b5feb6423b8a6a5a5de34d277844fba9
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-29 15:18:39 +0800

    :white_check_mark: Add test authentication flow

--- apps/ecommerce_starter_app/test/src/features/authentication/auth_flow_test.dart
+++ apps/ecommerce_starter_app/test/src/features/authentication/auth_flow_test.dart
@@ -7,5 +7,13 @@ void main() {
     final r = Robot(tester);
     await r.pumpMyApp();
     r.expectFindAllProductsCard();
+    await r.openPopupMenu();
+    await r.auth.openEmailPasswordSignInScreen();
+    await r.auth.signInWithEmailAndPassword();
+    r.expectFindAllProductsCard();
+    await r.auth.openAccountScreen();
+    await r.auth.tapLogoutButton();
+    await r.auth.tapDialogLogoutButton();
+    r.expectFindAllProductsCard();
   });
 }
