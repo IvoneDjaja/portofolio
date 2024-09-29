b7100c6ef5e4c91cae667fd69bdb413a030b3fde
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-09-29 22:10:27 +0800

    :white_check_mark: Add test golden image with size variants

--- apps/ecommerce_starter_app/test/src/goldens/golden_products_list_test.dart
+++ apps/ecommerce_starter_app/test/src/goldens/golden_products_list_test.dart
@@ -1,14 +1,31 @@
 import 'package:ecommerce_starter_app/src/app.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../robots.dart';
 
 void main() {
-  testWidgets('Golden - products list', (tester) async {
-    final r = Robot(tester);
-    await r.golden.loadRobotoFont();
-    await r.pumpMyApp();
-    await expectLater(
-        find.byType(MyApp), matchesGoldenFile('products_list.png'));
+  final sizeVariant = ValueVariant<Size>({
+    const Size(300, 600),
+    const Size(600, 800),
+    const Size(1000, 1000),
   });
+  testWidgets(
+    'Golden - products list',
+    (tester) async {
+      final r = Robot(tester);
+      final currentSize = sizeVariant.currentValue!;
+      await r.golden.setSurfaceSize(currentSize);
+      await r.golden.loadRobotoFont();
+      await r.pumpMyApp();
+      await expectLater(
+        find.byType(MyApp),
+        matchesGoldenFile(
+          'products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png',
+        ),
+      );
+    },
+    variant: sizeVariant,
+    tags: ['golden'],
+  );
 }
