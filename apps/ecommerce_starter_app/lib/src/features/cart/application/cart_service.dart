6a1924f16062f06f72df47102dcf158c2c176b23
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-10-05 20:03:50 +0800

    :sparkles: Read dependencies using Ref in CartService

--- apps/ecommerce_starter_app/lib/src/features/cart/application/cart_service.dart
+++ apps/ecommerce_starter_app/lib/src/features/cart/application/cart_service.dart
@@ -8,34 +8,28 @@ import 'package:ecommerce_starter_app/src/features/products/domain/product.dart'
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartService {
-  CartService({
-    required this.authRepository,
-    required this.localCartRepository,
-    required this.remoteCartRepository,
-  });
-  final FakeAuthRepository authRepository;
-  final LocalCartRepository localCartRepository;
-  final RemoteCartRepository remoteCartRepository;
+  CartService(this.ref);
+  final Ref ref;
 
   /// Fetch the cart from the local or remote repository depending on the user
   /// auth state.
   Future<Cart> _fetchCart() async {
-    final user = authRepository.currentUser;
+    final user = ref.read(authRepositoryProvider).currentUser;
     if (user != null) {
-      return remoteCartRepository.fetchCart(user.uid);
+      return ref.read(remoteCartRepositoryProvider).fetchCart(user.uid);
     } else {
-      return localCartRepository.fetchCart();
+      return ref.read(localCartRepositoryProvider).fetchCart();
     }
   }
 
   /// Save the cart to the local or remote repository depending on the user auth
   /// state.
   Future<void> _setCart(Cart cart) async {
-    final user = authRepository.currentUser;
+    final user = ref.read(authRepositoryProvider).currentUser;
     if (user != null) {
-      return remoteCartRepository.setCart(user.uid, cart);
+      return ref.read(remoteCartRepositoryProvider).setCart(user.uid, cart);
     } else {
-      return localCartRepository.setCart(cart);
+      return ref.read(localCartRepositoryProvider).setCart(cart);
     }
   }
 
@@ -61,9 +55,5 @@ class CartService {
 }
 
 final cartServiceProvider = Provider<CartService>((ref) {
-  return CartService(
-    authRepository: ref.watch(authRepositoryProvider),
-    localCartRepository: ref.watch(localCartRepositoryProvider),
-    remoteCartRepository: ref.watch(remoteCartRepositoryProvider),
-  );
+  return CartService(ref);
 });
