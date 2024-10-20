2635ddc1721fb90188e2dbf5977cff91fba68231
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-10-20 14:03:19 +0800

    :white_check_mark: Update widget and integration test

--- apps/ecommerce_starter_app/integration_test/authentication/auth_flow_test.dart
+++ apps/ecommerce_starter_app/integration_test/authentication/auth_flow_test.dart
@@ -1,21 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
-import '../test/src/robots.dart';
+import '../../test/src/robots.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('Sign in and sign out flow', (tester) async {
     final r = Robot(tester);
     await r.pumpMyApp();
-    r.expectFindAllProductsCard();
+    r.products.expectFindAllProductCards();
     await r.openPopupMenu();
     await r.auth.openEmailPasswordSignInScreen();
     await r.auth.signInWithEmailAndPassword();
-    r.expectFindAllProductsCard();
+    r.products.expectFindAllProductCards();
+    await r.openPopupMenu();
     await r.auth.openAccountScreen();
     await r.auth.tapLogoutButton();
     await r.auth.tapDialogLogoutButton();
-    r.expectFindAllProductsCard();
+    r.products.expectFindAllProductCards();
   });
 }
--- apps/ecommerce_starter_app/test/src/mocks.dart
+++ apps/ecommerce_starter_app/test/src/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_starter_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_starter_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_starter_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_starter_app/src/features/cart/data/remote/remote_cart_repository.dart';
 import 'package:ecommerce_starter_app/src/features/products/data/fake_products_repository.dart';
@@ -11,3 +12,9 @@ class MockRemoteCartRepository extends Mock implements RemoteCartRepository {}
 class MockLocalCartRepository extends Mock implements LocalCartRepository {}
 
 class MockProductsRepository extends Mock implements FakeProductsRepository {}
+
+class MockCartService extends Mock implements CartService {}
+
+class Listener<T> extends Mock {
+  void call(T? previous, T next);
+}
