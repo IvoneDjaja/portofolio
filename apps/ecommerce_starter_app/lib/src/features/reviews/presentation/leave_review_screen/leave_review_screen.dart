8563e3d65191cfd185f20b91f77f318790c6f458
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-11-03 06:50:49 +0800

    :sparkles: Add LeaveReviewController and submit form data

--- apps/ecommerce_starter_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
+++ apps/ecommerce_starter_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
@@ -1,8 +1,11 @@
 import 'package:ecommerce_starter_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_starter_app/src/constants/breakpoints.dart';
+import 'package:ecommerce_starter_app/src/features/checkout/presentation/payment/payment_button.dart';
 import 'package:ecommerce_starter_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_starter_app/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart';
 import 'package:ecommerce_starter_app/src/features/reviews/presentation/product_reviews/product_rating_bar.dart';
 import 'package:ecommerce_starter_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_starter_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_starter_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_starter_app/src/common_widgets/primary_button.dart';
@@ -63,6 +66,11 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
 
   @override
   Widget build(BuildContext context) {
+    ref.listen<AsyncValue>(
+      leaveReviewControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+    final state = ref.watch(leaveReviewControllerProvider);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -93,13 +101,16 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
         gapH32,
         PrimaryButton(
           text: 'Submit'.hardcoded,
-          // TODO: Loading state
-          isLoading: false,
+          isLoading: state.isLoading,
           onPressed: _rating == 0
               ? null
-              // TODO: submit review
-              : () => showNotImplementedAlertDialog(context: context),
-        )
+              : () =>
+                  ref.read(leaveReviewControllerProvider.notifier).submitReview(
+                        productId: widget.productId,
+                        rating: _rating,
+                        comment: _controller.text,
+                      ),
+        ),
       ],
     );
   }
