eeeadc63bb99c20661322d1ed12d7c4ab80ac685
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-11-02 18:32:29 +0800

    :sparkles: Add AsyncErrorLogger using ProviderObserver

--- apps/ecommerce_starter_app/lib/main.dart
+++ apps/ecommerce_starter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_starter_app/src/app.dart';
+import 'package:ecommerce_starter_app/src/exceptions/async_error_logger.dart';
 import 'package:ecommerce_starter_app/src/features/cart/application/cart_sync_service.dart';
 import 'package:ecommerce_starter_app/src/features/cart/data/local/sembast_cart_repository.dart';
 import 'package:ecommerce_starter_app/src/localization/string_hardcoded.dart';
@@ -16,11 +17,11 @@ void main() async {
   registerErrorHandlers();
   final localCartRepository = await SembastCartRepository.makeDefault();
   // * Create ProviderContainer with any required overrides
-  final container = ProviderContainer(
-    overrides: [
-      localCartRepositoryProvider.overrideWithValue(localCartRepository),
-    ],
-  );
+  final container = ProviderContainer(overrides: [
+    localCartRepositoryProvider.overrideWithValue(localCartRepository),
+  ], observers: [
+    AsyncErrorLogger(),
+  ]);
   // * Initialize CartSyncService to start the listener
   container.read(cartSyncServiceProvider);
   // * Entry point of the app
