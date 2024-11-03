59792ab34d1df8a333e3ebc9cb732bb43996b4be
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2024-11-04 07:19:41 +0800

    ecommerce_starter_app: :sparkles: Convert AccountScreenController to use riverpod generator

--- apps/ecommerce_starter_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ apps/ecommerce_starter_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -2,8 +2,12 @@ import 'dart:async';
 
 import 'package:ecommerce_starter_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class AccountScreenController extends AutoDisposeAsyncNotifier<void> {
+part 'account_screen_controller.g.dart';
+
+@riverpod
+class AccountScreenController extends _$AccountScreenController {
   @override
   FutureOr<void> build() {
     // nothing to do
@@ -15,8 +19,3 @@ class AccountScreenController extends AutoDisposeAsyncNotifier<void> {
     state = await AsyncValue.guard(() => authRepository.signOut());
   }
 }
-
-final accountScreenControllerProvider =
-    AutoDisposeAsyncNotifierProvider<AccountScreenController, void>(
-  AccountScreenController.new,
-);
