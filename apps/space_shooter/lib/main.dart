11cb8afa8f335c7d41312cb6aeebccea3b2fc21f
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2025-01-12 22:22:34 +0800

    fix(space_shooter): :sparkles: Add control player and graphic

--- apps/space_shooter/lib/main.dart
+++ apps/space_shooter/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:space_shooter/player.dart';
 
-class SpaceShooterGame extends FlameGame {
+class SpaceShooterGame extends FlameGame with PanDetector {
+  late Player player;
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    player = Player();
+    add(player);
+  }
 
-    add(Player()
-      ..position = size / 2
-      ..width = 50
-      ..height = 100
-      ..anchor = Anchor.center);
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    player.move(info.delta.global);
   }
 }
 
--- apps/space_shooter/lib/player.dart
+++ apps/space_shooter/lib/player.dart
@@ -1,11 +1,25 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'package:space_shooter/main.dart';
 
-class Player extends PositionComponent {
+class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
   static final _paint = Paint()..color = Colors.white;
 
+  Player()
+      : super(
+          size: Vector2(100, 150),
+          anchor: Anchor.center,
+        );
+
   @override
-  void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _paint);
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    sprite = await gameRef.loadSprite('player-sprite.png');
+    position = gameRef.size / 2;
+  }
+
+  void move(Vector2 delta) {
+    position.add(delta);
   }
 }
